ccbfd0c23e224ee2ab8773cb4774d2ab880c4a45
Talinee <bieteuy@gmail.com>
2021-08-20 16:25:13 +0700

    edit package

--- src/api/rest/routes/index.ts
+++ src/api/rest/routes/index.ts
@@ -1,14 +1,7 @@
 import { Express } from 'express'
-import reward from './reward'
-import news from './news'
 import auth from './auth'
 import account from './account'
-import knowledge from './knowledge'
-import event from './event'
-import favourite from './favorite'
-import category from './category'
-import myEvent from './myEvent'
 
 export default (app: Express) => {
-    app.use('/api', [ reward, news, auth, account, knowledge, event, favourite, category, myEvent ])
+    app.use('/api', [ auth, account ])
 }
